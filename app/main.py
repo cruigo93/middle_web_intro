203bb460d0594c8ddfda71427bf5815e2eeefbd4
Zhuldyzzhan <cruigo93@gmail.com>
2022-08-04 21:02:10 +0600

    refactor: pep8 changes in main.py and test

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 """Генератор приветствий."""
 import pprint
 
+
 def Greeting(name: str) -> str:
-      """Возвращает текст приветствия.
+    """Возвращает текст приветствия.
 
-      Args:
-          name: Имя пользователя
+    Args:
+        name: Имя пользователя
 
-      Returns:
-          int: Текст приветствия
-      """
-      # pprint.pprint(name.lower())
-      return 'Привет, name'
+    Returns:
+        int: Текст приветствия
+    """
+    # pprint.pprint(name.lower())
+    return 'Привет, name'
